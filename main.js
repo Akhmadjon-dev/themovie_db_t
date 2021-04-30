a41c8d9e242a6ed7808b358c9cc1ab0d3fc69982
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-04-30 21:57:47 +0500

    header bug fixed

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-var film = document.querySelector('#film');
+// var film = document.querySelector("#film");
 
-function clearCoor() {
-  document.querySelector(".header__film_dropdown").innerHTML = "";
-}
-function myFunction(event){
-  var block = document.querySelector(".header__film_dropdown");
-  block.style.display="block";
-  document.block.innerHTML = clearCore();
-}
-
+// function clearCoor() {
+//   document.querySelector(".header__film_dropdown").style.display = "none";
+// }
+// function myFunction(event) {
+//   var block = document.querySelector(".header__film_dropdown");
+//   block.style.display = "block";
+//   // document.block.innerHTML = clearCore();
+// }
