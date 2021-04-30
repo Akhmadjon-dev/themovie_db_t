d8cdf97ef75279472dbdbb7432fce3e666a39e95
Alishergoold <alishergoold1989@gmail.com>
2021-04-30 21:28:41 +0500

    Headerni yasadim.

---
+++ main.js
@@ -0,0 +1,11 @@
+var film = document.querySelector('#film');
+
+function clearCoor() {
+  document.querySelector(".header__film_dropdown").innerHTML = "";
+}
+function myFunction(event){
+  var block = document.querySelector(".header__film_dropdown");
+  block.style.display="block";
+  document.block.innerHTML = clearCore();
+}
+
